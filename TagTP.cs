97803a85c1b9c7f3fd696a9a30cd19f3f0cfccf8
Reava <93742413+Reava@users.noreply.github.com>
2021-12-17 22:18:06 +0100

    Update TagTP.cs

--- TagTP.cs
+++ TagTP.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using VRC.SDK3.Components;
 using UdonSharp;
 using VRC.SDKBase;
@@ -8,18 +8,29 @@ namespace UdonSharp.Examples.Utilities
 {
     public class InteractToggleTag : UdonSharpBehaviour
     {
-        [Tooltip("Tag name allowed")]
-        public string tag;
-        [Tooltip("TP Target")]
+        [Header("Tag name allowed")]
+        public string tagAllowed;
+        [Header("TP Target when allowed")]
         public Transform targetLocation;
+        [Header("TP elsewhere when tag doesn't match?")]
+        public bool tpOption;
+        [Header("TP Target when disallowed")]
+        public Transform targetLocation2;
 
         public override void Interact()
         {
-            if (Networking.LocalPlayer.GetPlayerTag("rank") == tag)
+            if (Networking.LocalPlayer != null)
             {
-                if (Networking.LocalPlayer != null)
+                if (Networking.LocalPlayer.GetPlayerTag("rank") == tagAllowed)
                 {
                     Networking.LocalPlayer.TeleportTo(targetLocation.position, targetLocation.rotation);
+                }
+                else
+                {
+                    if (tpOption)
+                    {
+                        Networking.LocalPlayer.TeleportTo(targetLocation2.position, targetLocation2.rotation);
+                    }
                 }
             }
         }
