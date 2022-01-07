f82bcde3ad1b11fece1c51b87c1237885fa5d19d
Reava <93742413+Reava@users.noreply.github.com>
2022-01-07 14:31:17 +0100

    Create UnityFogToggle.cs

---
+++ UnityFogToggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Fog : UdonSharpBehaviour
+{
+    public bool fog_Default = true;
+    void Start()
+    {
+        RenderSettings.fog = fog_Default;
+    }
+    public void Interact()
+    {
+        if (RenderSettings.fog)
+        {
+            RenderSettings.fog = false;
+        }
+        else
+        {
+            RenderSettings.fog = true;
+        }
+    }
+}
