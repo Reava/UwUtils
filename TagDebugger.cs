6fba511946df56ff07accfb14170be16d6cf3b29
Reava <93742413+Reava@users.noreply.github.com>
2022-02-08 17:25:21 +0100

    Create TagDebugger.cs

---
+++ TagDebugger.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+using TMPro;
+using UnityEngine.UI;
+
+public class TagDebugger : UdonSharpBehaviour
+{
+    private string playerTag;
+    [Header("interacting with this behavior will update the display & log.")]
+    public GameObject TMP_Display;
+    public bool usingTMP = false;
+    [Header("Is the TMP text GUI or classic ?")]
+    public bool GUI_Text = false;
+    [Header("Output debug logs?")]
+    public bool debugEnabled = true;
+    void Start()
+    {
+        updateDebugDisplay();
+    }
+
+    public override void Interact()
+    {
+        updateDebugDisplay();
+    }
+
+    public void updateDebugDisplay()
+    {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        if (Networking.LocalPlayer.GetPlayerTag("rank") != null)
+        {
+            var playerTag = Networking.LocalPlayer.GetPlayerTag("rank");
+            if (debugEnabled)
+            {
+                Debug.Log(playerTag);
+            }
+            if (usingTMP)
+            {
+                if (GUI_Text)
+                {
+                    TMP_Display.GetComponent<TextMeshProUGUI>().text = playerTag;
+                }
+                else
+                {
+                    TMP_Display.GetComponent<TextMeshPro>().text = playerTag;
+                }
+            }
+            else
+            {
+                TMP_Display.GetComponent<Text>().text = playerTag;
+            }
+        }
+        else
+        {
+            if (debugEnabled)
+            {
+                Debug.Log("No tag detected.");
+            }
+            if (usingTMP)
+            {
+                if (GUI_Text)
+                {
+                    TMP_Display.GetComponent<TextMeshProUGUI>().text = "No tag detected";
+                }
+                else
+                {
+                    TMP_Display.GetComponent<TextMeshPro>().text = "No tag detected";
+                }
+            }
+            else
+            {
+                TMP_Display.GetComponent<Text>().text = "No tag detected";
+            }
+        }
+    }
+}
