bb74af15d7ae93b44ab21067f5c3260bed6c3f06
Reava <93742413+Reava@users.noreply.github.com>
2022-02-17 13:50:30 +0100

    Add files via upload

---
+++ iTP.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+public class iTP : UdonSharpBehaviour
+{
+    public Transform targetLocation;
+
+    public override void Interact()
+    {
+        if (Networking.LocalPlayer != null)
+        {
+            Networking.LocalPlayer.TeleportTo(targetLocation.position, targetLocation.rotation);
+        }
+    }
+}
