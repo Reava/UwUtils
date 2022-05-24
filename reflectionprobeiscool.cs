bc8a8ed7da391e0cfc578f20c2c006f934ed2600
Reava <93742413+Reava@users.noreply.github.com>
2022-05-24 16:58:14 +0200

    Update reflectionprobeiscool.cs

--- reflectionprobeiscool.cs
+++ reflectionprobeiscool.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
 public class reflectionprobeiscool : UdonSharpBehaviour
 {
     #region References
