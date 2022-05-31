59c39125ca0b9473bffb3cf6468b44b6312efaa9
Reava <eva@xprlabs.com>
2022-05-31 23:16:19 +0200

    Fixed includes & Sync modes

--- Scripts/Source/TagAssigner.cs
+++ Scripts/Source/TagAssigner.cs
@@ -1,60 +1,68 @@
 using UnityEngine;
 using VRC.SDKBase;
+using UdonSharp;
 
-namespace UdonSharp.Examples.Utilities
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class TagAssigner : UdonSharpBehaviour
 {
-    public class TagAssigner : UdonSharpBehaviour
+    [Tooltip("Name of the tag")]
+    public string playerTag;
+    [Tooltip("List of users who will inherit the tag")]
+    public string[] userArray;
+    [Tooltip("List of objects to toggle ON for VIPs")]
+    public GameObject[] toggleObjectsON;
+    [Tooltip("List of objects to toggle OFF for VIPs")]
+    public GameObject[] toggleObjectsOFF;
+    public bool tpPlayerOnJoin = true;
+    public Transform tpLocation;
+    private float delay = 2f;
+    void Start()
     {
-        [Tooltip("Name of the tag")]
-        public string playerTag;
-        [Tooltip("List of users who will inherit the tag")]
-        public string[] userArray;
-        [Tooltip("List of objects to toggle ON for VIPs")]
-        public GameObject[] toggleObjectsON;
-        [Tooltip("List of objects to toggle OFF for VIPs")]
-        public GameObject[] toggleObjectsOFF;
-        public bool tpPlayerOnJoin = true;
-        public Transform tpLocation;
-        void Start()
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        for (int i = 0; i < userArray.Length; i++)
         {
-            VRCPlayerApi localPlayer = Networking.LocalPlayer;
-            for (int i = 0; i < userArray.Length; i++)
+            if (userArray[i] == localPlayer.displayName)
             {
-                if (userArray[i] == localPlayer.displayName)
+                localPlayer.SetPlayerTag("rank", playerTag);
+                if (tpPlayerOnJoin)
                 {
-                    localPlayer.SetPlayerTag("rank", playerTag);
-                    if (tpPlayerOnJoin)
-                    {
-                        Networking.LocalPlayer.TeleportTo(tpLocation.position, tpLocation.rotation);
-                    }
-                    foreach (GameObject toggleObjectON in toggleObjectsON)
-                    {
-                        toggleObjectON.SetActive(true);
-                    }
-                    foreach (GameObject toggleObjectOFF in toggleObjectsOFF)
-                    {
-                        toggleObjectOFF.SetActive(false);
-                    }
-                    break;
+                    SendCustomEventDelayedSeconds(nameof(_initTeleport), delay);
                 }
-                else
+                foreach (GameObject toggleObjectON in toggleObjectsON)
+                {
+                    toggleObjectON.SetActive(true);
+                }
+                foreach (GameObject toggleObjectOFF in toggleObjectsOFF)
+                {
+                    toggleObjectOFF.SetActive(false);
+                }
+                break;
+            }
+            else
+            {
+                if(Networking.LocalPlayer.GetPlayerTag("rank") == null)
                 {
                     localPlayer.SetPlayerTag("rank", "Visitor");
                 }
             }
         }
-        public void _updateState()
+    }
+    public void _updateState()
+    {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        localPlayer.SetPlayerTag("rank", playerTag);
+        foreach (GameObject toggleObject in toggleObjectsON)
         {
-            VRCPlayerApi localPlayer = Networking.LocalPlayer;
-            localPlayer.SetPlayerTag("rank", playerTag);
-            foreach (GameObject toggleObject in toggleObjectsON)
-            {
-                toggleObject.SetActive(true);
-            }
-            foreach (GameObject toggleObject in toggleObjectsOFF)
-            {
-                toggleObject.SetActive(false);
-            }
+            toggleObject.SetActive(true);
         }
+        foreach (GameObject toggleObject in toggleObjectsOFF)
+        {
+            toggleObject.SetActive(false);
+        }
+    }
+
+    public void _initTeleport()
+    {
+        Networking.LocalPlayer.TeleportTo(tpLocation.position, tpLocation.rotation);
     }
 }
--- Scripts/Source/TagTP.cs
+++ Scripts/Source/TagTP.cs
@@ -1,33 +1,32 @@
 using UnityEngine;
 using VRC.SDKBase;
+using UdonSharp;
 
-namespace UdonSharp.Examples.Utilities
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class TagTP : UdonSharpBehaviour
 {
-    public class TagTP : UdonSharpBehaviour
-    {
-        [Header("Tag name allowed")]
-        public string tagAllowed;
-        [Header("TP Target when allowed")]
-        public Transform targetLocation;
-        [Header("TP elsewhere when tag doesn't match?")]
-        public bool tpOption;
-        [Header("TP Target when disallowed")]
-        public Transform targetLocation2;
+    [Header("Tag name allowed")]
+    public string tagAllowed;
+    [Header("TP Target when allowed")]
+    public Transform targetLocation;
+    [Header("TP elsewhere when tag doesn't match?")]
+    public bool tpOption;
+    [Header("TP Target when disallowed")]
+    public Transform targetLocation2;
 
-        public override void Interact()
+    public override void Interact()
+    {
+        if (Networking.LocalPlayer != null)
         {
-            if (Networking.LocalPlayer != null)
+            if (Networking.LocalPlayer.GetPlayerTag("rank") == tagAllowed)
             {
-                if (Networking.LocalPlayer.GetPlayerTag("rank") == tagAllowed)
+                Networking.LocalPlayer.TeleportTo(targetLocation.position, targetLocation.rotation);
+            }
+            else
+            {
+                if (tpOption)
                 {
-                    Networking.LocalPlayer.TeleportTo(targetLocation.position, targetLocation.rotation);
-                }
-                else
-                {
-                    if (tpOption)
-                    {
-                        Networking.LocalPlayer.TeleportTo(targetLocation2.position, targetLocation2.rotation);
-                    }
+                    Networking.LocalPlayer.TeleportTo(targetLocation2.position, targetLocation2.rotation);
                 }
             }
         }
