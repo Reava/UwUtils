99444d21a87ddcf18e70a93b09402899ad727e47
Reava <reava.vr@gmail.com>
2022-08-08 01:14:10 +0200

    Create iState.cs

---
+++ Scripts/iState.cs
@@ -0,0 +1,35 @@
+﻿using UdonSharp;
+using Unity.Collections;
+using UnityEngine;
+using VRC.SDK3.Components;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class iState : UdonSharpBehaviour
+{
+    [Header("List of objects")]
+    public GameObject[] toggleObjects;
+
+    public override void Interact()
+    {
+        foreach (GameObject toggleObject in toggleObjects)
+        {
+            toggleObject.SetActive(!toggleObject.activeSelf);
+        }
+    }
+
+    public void _Enable()
+    {
+        foreach (GameObject toggleObject in toggleObjects)
+        {
+            toggleObject.SetActive(true);
+        }
+    }
+
+    public void _Disable()
+    {
+        foreach (GameObject toggleObject in toggleObjects)
+        {
+            toggleObject.SetActive(false);
+        }
+    }
+}
