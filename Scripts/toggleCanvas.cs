4e0d39204e29f2dc02a55eea4ec1b1e491660971
Reava <reava.vr@gmail.com>
2022-08-08 01:19:03 +0200

    Create toggleCanvas.cs

---
+++ Scripts/toggleCanvas.cs
@@ -0,0 +1,35 @@
+﻿using UdonSharp;
+using Unity.Collections;
+using UnityEngine;
+using VRC.SDK3.Components;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class toggleCanvas : UdonSharpBehaviour
+{
+    [Header("List of objects")]
+    public Canvas[] canvasesToToggle;
+
+    public override void Interact()
+    {
+        foreach (Canvas toggleObject in canvasesToToggle)
+        {
+            toggleObject.enabled = !toggleObject.enabled;
+        }
+    }
+
+    public void _Enable()
+    {
+        foreach (Canvas toggleObject in canvasesToToggle)
+        {
+            toggleObject.enabled = true;
+        }
+    }
+
+    public void _Disable()
+    {
+        foreach (Canvas toggleObject in canvasesToToggle)
+        {
+            toggleObject.enabled = false;
+        }
+    }
+}
