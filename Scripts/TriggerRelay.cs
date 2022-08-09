3dd674339715b4c9b5831c6eb3ebb1101ad0aa66
Reava <reava.vr@gmail.com>
2022-08-09 09:12:43 +0200

    Multiple fixes & U# 1.x+ upgrade

--- Scripts/TriggerRelay.cs
+++ Scripts/TriggerRelay.cs
@@ -12,48 +12,41 @@ public class TriggerRelay : UdonSharpBehaviour
     [Header("Event settings")]
     [SerializeField] private bool onEnter = false;
     [SerializeField] private bool onExit = false;
-    [SerializeField] private UdonBehaviour eventTarget;
+    [SerializeField] private UdonBehaviour[] eventTarget;
     [SerializeField] private string eventName = "_interact";
     private bool valid = false;
 
     void Start()
     {
-        /*foreach(Collider col in TriggerColliders)
-        {
-            if(col != null)
-            {
-                valid = true;
-                continue;
-            }
-            else
-            {
-                Debug.Log("[UwUtils/TriggerRelay.cs] Collider array is invalid for object '" + gameObject.name + "'");
-                break;
-            }
-        }*/
-        if(/*valid = true && */eventTarget != null)
+        if(eventTarget != null)
         {
             valid = true;
         }
         else
         {
             valid = false;
-            Debug.Log("[UwUtils/TriggerRelay.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
+            Debug.LogError("[UwUtils/TriggerRelay.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
         }
     }
 
     public override void OnPlayerTriggerExit(VRCPlayerApi player)
     {
-        if (valid)
+        if (valid && onExit)
         {
-            eventTarget.SendCustomEvent("_interact");
+            foreach(UdonBehaviour program in eventTarget)
+            {
+                program.SendCustomEvent(eventName);
+            }
         }
     }
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
         if (valid && onEnter)
         {
-            eventTarget.SendCustomEvent("_interact");
+            foreach (UdonBehaviour program in eventTarget)
+            {
+                program.SendCustomEvent(eventName);
+            }
         }
     }
 }
