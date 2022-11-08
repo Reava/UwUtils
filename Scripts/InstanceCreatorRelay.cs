d28ea7955ad87e9eeeeeea970b74484ba3c8ffca
Reava <reava.vr@gmail.com>
2022-11-08 07:24:19 +0100

    Create InstanceCreatorRelay.cs

---
+++ Scripts/InstanceCreatorRelay.cs
@@ -0,0 +1,25 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class InstanceCreatorRelay : UdonSharpBehaviour
+{
+    [Header("Name of the event")]
+    [SerializeField] private string eventName = "_interact";
+    [Header("Programs to send a custom event to")]
+    [SerializeField] private UdonBehaviour[] programsList;
+
+    void Start()
+    {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        if (Networking.LocalPlayer.isMaster)
+        {
+            foreach (UdonBehaviour b in programsList)
+            {
+                b.SendCustomEvent(eventName);
+            }
+        }
+    }
+}
