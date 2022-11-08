897a7e3dd6e22f5531db0bb8a4b02707748995e1
Reava <reava.vr@gmail.com>
2022-11-08 07:24:26 +0100

    Update TagAssigner.cs

--- Scripts/TagAssigner.cs
+++ Scripts/TagAssigner.cs
@@ -1,34 +1,62 @@
 using UnityEngine;
 using VRC.SDKBase;
 using UdonSharp;
+using VRC.Udon;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class TagAssigner : UdonSharpBehaviour
 {
-    [Tooltip("Name of the tag")]
-    [SerializeField] private string playerTag;
-    [Tooltip("List of users who will inherit the tag")]
+    [Header("Name of the tag to assign to VIPs")]
+    [SerializeField] private string playerTag = "vip";
+    [Header("List of users who gets 'VIP'")]
     [SerializeField] private string[] userArray;
-    [Tooltip("List of objects to toggle ON for VIPs")]
+    [Header("List of objects to toggle ON for VIPs")]
     [SerializeField] private GameObject[] toggleObjectsON;
-    [Tooltip("List of objects to toggle OFF for VIPs")]
+    [Header("List of objects to toggle OFF for VIPs")]
     [SerializeField] private GameObject[] toggleObjectsOFF;
+    [Header("Send custom event to these behaviors if local user is VIP")]
+    [SerializeField] private UdonBehaviour[] programsSuccess;
+    [Header("Name of the custom event")]
+    [SerializeField] private string eventName = "_interact";
     [SerializeField] private bool tpPlayerOnJoin = true;
     [SerializeField] private Transform tpLocation;
-    private float delay = 0.2f;
+    [Header("Consider instance creator as VIP")]
+    [SerializeField] private bool EmpowerInstanceCreator = false;
+    private bool empoweredUser = false;
+    private float delay = 0.6f;
     private bool abort;
     void Start()
     {
-        if (playerTag == null)
+        if (playerTag == null) //Checks for valid tag
         {
             abort = true;
             SendCustomEventDelayedSeconds(nameof(_sendDebugError), 1f);
             return;
         }
         VRCPlayerApi localPlayer = Networking.LocalPlayer;
-        for (int i = 0; i < userArray.Length; i++)
+        if (Networking.LocalPlayer.isMaster && EmpowerInstanceCreator) //Empowers instance creator if enabled
         {
-            if (userArray[i] == localPlayer.displayName)
+            localPlayer.SetPlayerTag("rank", playerTag);
+            foreach (GameObject toggleObjectON in toggleObjectsON)
+            {
+                toggleObjectON.SetActive(true);
+            }
+            foreach (GameObject toggleObjectOFF in toggleObjectsOFF)
+            {
+                toggleObjectOFF.SetActive(false);
+            }
+            if (programsSuccess[0] != null && !programsSuccess[0].Equals(null))
+            {
+                foreach (UdonBehaviour b in programsSuccess)
+                {
+                    b.SendCustomEvent(eventName);
+                }
+            }
+            return;
+        }
+        for (int i = 0; i < userArray.Length; i++) //Checks user array for matches with local user to empower
+        {
+            if (userArray[i] == localPlayer.displayName || empoweredUser)
             {
                 localPlayer.SetPlayerTag("rank", playerTag);
                 if (tpPlayerOnJoin)
@@ -43,18 +71,30 @@ public class TagAssigner : UdonSharpBehaviour
                 {
                     toggleObjectOFF.SetActive(false);
                 }
+                if (programsSuccess[0] != null && !programsSuccess[0].Equals(null))
+                {
+                    foreach (UdonBehaviour b in programsSuccess)
+                    {
+                        b.SendCustomEvent(eventName);
+                    }
+                }
                 break;
             }
             else
             {
-                if(Networking.LocalPlayer.GetPlayerTag("rank") == null)
+                if (Networking.LocalPlayer.GetPlayerTag("rank") == null)
                 {
                     localPlayer.SetPlayerTag("rank", "Visitor");
                 }
             }
         }
     }
-    public void _updateState()
+    public void _addNewUser() //just an alias
+    {
+        _updateState();
+    }
+
+    public void _updateState() //adds a user to VIPs on calling this function
     {
         if (abort) return;
         VRCPlayerApi localPlayer = Networking.LocalPlayer;
