81dba2de345c74597364fedaa88c5eb1cb65d464
Reava <reava.vr@gmail.com>
2023-01-22 17:12:11 +0100

    Create FadingTP Assets

---
+++ FadingTP/Scripts/DynamicCallback.cs
@@ -0,0 +1,25 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class DynamicCallback : UdonSharpBehaviour
+{
+    [HideInInspector] public UdonSharpBehaviour tscript;
+
+    public override void Interact()
+    {
+        if (tscript != null) tscript.SendCustomEvent("_teleportPlayer");
+    }
+
+    public void _sendCallback()
+    {
+        tscript.SendCustomEvent("_teleportPlayer");
+    }
+
+    public void _selectedOutput(GameObject targetScript)
+    {
+        tscript = targetScript.GetComponent<UdonSharpBehaviour>();
+    }
+}
