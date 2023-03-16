f4de94b31098c863c25db34ccba27281e4a1656f
Reava <reava.vr@gmail.com>
2023-03-16 09:13:44 +0100

    v0.90

--- SpawnFade/Scripts/OnSpawnFade.cs
+++ SpawnFade/Scripts/OnSpawnFade.cs
@@ -4,32 +4,36 @@ using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine.UI;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-public class OnSpawnFade : UdonSharpBehaviour
+namespace UwUtils
 {
-    [Range(0f, 3f)]
-    [SerializeField] private float FadeSpeed = 1f;
-    [SerializeField] private GameObject AnimatorParent;
-    [SerializeField] private bool FadeOnRespawn = true;
-    private Animator AnimatorTarget;
-
-    public void Start()
+    [AddComponentMenu("UwUtils/On Spawn Fade")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OnSpawnFade : UdonSharpBehaviour
     {
-        if (!AnimatorParent) _sendDebugError();
-        AnimatorTarget = AnimatorParent.GetComponent<Animator>();
-        AnimatorTarget.SetFloat("fadeSpeed", FadeSpeed);
-        AnimatorTarget.SetBool("fade", true);
-    }
+        [Range(0f, 3f)]
+        [SerializeField] private float FadeSpeed = 1f;
+        [SerializeField] private GameObject AnimatorParent;
+        [SerializeField] private bool FadeOnRespawn = true;
+        private Animator AnimatorTarget;
 
-    public void OnPlayerRespawn(VRCPlayerApi player)
-    {
-        AnimatorTarget.SetFloat("fadeSpeed", FadeSpeed);
-        if (FadeOnRespawn) AnimatorTarget.SetBool("fade", true);
-    }
+        public void Start()
+        {
+            if (!AnimatorParent) _sendDebugError();
+            AnimatorTarget = AnimatorParent.GetComponent<Animator>();
+            AnimatorTarget.SetFloat("fadeSpeed", FadeSpeed);
+            AnimatorTarget.SetBool("fade", true);
+        }
 
-    public void _setValueback()
-    {
-        AnimatorTarget.SetBool("fade", false);
+        public void OnPlayerRespawn(VRCPlayerApi player)
+        {
+            AnimatorTarget.SetFloat("fadeSpeed", FadeSpeed);
+            if (FadeOnRespawn) AnimatorTarget.SetBool("fade", true);
+        }
+
+        public void _setValueback()
+        {
+            AnimatorTarget.SetBool("fade", false);
+        }
+        private void _sendDebugError() => Debug.LogError("Reava_UwUtils:<color=red> <b>Invalid references</b></color>, please review <color=orange>references</color> on: " + gameObject + ".", gameObject);
     }
-    private void _sendDebugError() => Debug.LogError("Reava_UwUtils:<color=red> <b>Invalid references</b></color>, please review <color=orange>references</color> on: " + gameObject + ".", gameObject);
 }
