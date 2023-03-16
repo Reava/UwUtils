c450ee1ac140114fa152f3fbee71d0149fc2b929
Reava <reava.vr@gmail.com>
2023-03-16 08:24:24 +0100

    Create SequencialToggle.cs

---
+++ Scripts/SequencialToggle.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.Udon;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Sequencial Toggle")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SequencialToggle : UdonSharpBehaviour
+    {
+        [Space]
+        [Header("Sequencial Toggle")]
+        [SerializeField] GameObject[] Targets;
+        [Space]
+        [SerializeField] bool enableLogging = true;
+
+        void Start()
+        {
+            //do the checks
+        }
+
+        public override void Interact()
+        {
+            //do the thing
+        }
+    }
+}
