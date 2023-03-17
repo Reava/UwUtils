42916e1566022686389f979882ee73f2eda96cca
Reava <reava.vr@gmail.com>
2023-03-17 05:05:23 +0100

    Renaming

--- Scripts/TriggerZoneRelay.cs
+++ Scripts/TriggerZoneRelay.cs
@@ -5,19 +5,27 @@ using VRC.Udon;
 
 namespace UwUtils
 {
-    [AddComponentMenu("UwUtils/TriggerRelay")]
+    [AddComponentMenu("UwUtils/TriggerZone Relay")]
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class TriggerRelay : UdonSharpBehaviour
+    public class TriggerZoneRelay : UdonSharpBehaviour
     {
+        [Space]
         [Header("Make your colliders Trigger & on the layer IgnoreRaycast")]
+        [SerializeField] private bool LocalPlayerOnly = true;
+        [Space]
         [Header("Make sure your colliders are on the same gameObject as this script")]
+        [Space]
         [SerializeField] private string eventNameOnExit = "_interact";
         [SerializeField] private string eventNameOnEnter = "_interact";
-        //[SerializeField] private Collider[] TriggerColliders;
+        [Space]
         [Header("Event settings")]
+        [Space]
         [SerializeField] private bool onEnter = true;
         [SerializeField] private bool onExit = false;
+        [Space]
         [SerializeField] private UdonBehaviour[] eventTargets;
+        [Space]
+        [SerializeField] private bool enableLogging = true;
         private bool valid = false;
 
         void Start()
@@ -29,12 +37,14 @@ namespace UwUtils
             else
             {
                 valid = false;
-                Debug.LogError("[UwUtils/TriggerRelay.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
+                Debug.LogError("[Reava_/UwUtils/TriggerZoneRelay.cs]: Setup is invalid, check your references for object '" + gameObject.name + "'", gameObject);
             }
         }
 
         public override void OnPlayerTriggerExit(VRCPlayerApi player)
         {
+            if (LocalPlayerOnly && player != Networking.LocalPlayer) return;
+            if (enableLogging) Debug.Log("[Reava_/UwUtils/TriggerZoneRelay.cs]: Player exit " + gameObject.name, gameObject);
             if (valid && onExit)
             {
                 foreach (UdonBehaviour program in eventTargets)
@@ -45,6 +55,8 @@ namespace UwUtils
         }
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
         {
+            if (LocalPlayerOnly && player != Networking.LocalPlayer) return;
+            if (enableLogging) Debug.Log("[Reava_/UwUtils/TriggerZoneRelay.cs]: Player entered " + gameObject.name, gameObject);
             if (valid && onEnter)
             {
                 foreach (UdonBehaviour program in eventTargets)
