a4bd4aef3eb0247e944a7633759b020b8a2b2bab
Reava <reava.vr@gmail.com>
2023-03-20 05:28:30 +0100

    Create SequentialToggle

--- Scripts/SequencialToggle.cs
+++ Scripts/SequencialToggle.cs
@@ -10,19 +10,49 @@ namespace UwUtils
     public class SequencialToggle : UdonSharpBehaviour
     {
         [Space]
-        [Header("Sequencial Toggle")]
+        [Header("Sequencial Toggle, Interact for next, _Toggle to toggle entirely")]
         [SerializeField] GameObject[] Targets;
         [Space]
         [SerializeField] bool enableLogging = true;
 
+        private int arrayLength = 0;
+        private int currentToggle = 0;
+        private bool systemState = true;
+
         void Start()
         {
-            //do the checks
+            if(Targets == null)
+            {
+                if (enableLogging) Debug.Log("[Reava_/UwUtils/SequentialToggle.cs]: No target objects found, disabling behaviour on " + gameObject.name + "", gameObject);
+                return;
+            }
+            foreach (GameObject t in Targets)
+            {
+                if (t == null) continue;
+                t.SetActive(false);
+            }
+            Targets[0].SetActive(true);
+            arrayLength = Targets.Length + 1;
         }
 
         public override void Interact()
         {
-            //do the thing
+            currentToggle += 1;
+            for (int i = 0; i < Targets.Length; i++)
+            {
+                if (i == currentToggle) Targets[i].SetActive(true);
+                Targets[i].SetActive(false);
+            }
+        }
+
+        public void _Toggle()
+        {
+            systemState = !systemState;
+            foreach (GameObject t in Targets)
+            {
+                if (t == null) continue;
+                t.SetActive(systemState);
+            }
         }
     }
 }
