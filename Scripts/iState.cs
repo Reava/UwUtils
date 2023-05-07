ae6adf15bb6f67306f2f18a8b5a5e878cff80f2b
Reava <reava.vr@gmail.com>
2023-05-07 19:53:43 +0200

    Add iState script back in

---
+++ Scripts/iState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UdonSharp;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/iState")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class iState : UdonSharpBehaviour
+    {
+        [Header("List of objects to toggle")]
+        [SerializeField] private GameObject[] toggleObjectsON;
+        private bool valid = true;
+        void Start()
+        {
+            if (toggleObjectsON.Length == 0)
+            {
+                valid = false;
+                Debug.LogError("[UwUtils/iState.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public override void Interact()
+        {
+            foreach (GameObject toggleObject in toggleObjectsON)
+            {
+                toggleObject.SetActive(!toggleObject.activeSelf);
+            }
+        }
+
+        public void _EnableAll()
+        {
+            foreach (GameObject toggleObject in toggleObjectsON)
+            {
+                toggleObject.SetActive(true);
+            }
+        }
+        public void _DisableAll()
+        {
+            foreach (GameObject toggleObject in toggleObjectsON)
+            {
+                toggleObject.SetActive(false);
+            }
+        }
+
+        public void _InvertState()
+        {
+            foreach (GameObject toggleObject in toggleObjectsON)
+            {
+                toggleObject.SetActive(!toggleObject.activeSelf);
+            }
+        }
+    }
+}
