f64096f78780b27e9ef4bd47dc0a1fd506d15e78
Reava <reava.vr@gmail.com>
2025-04-21 17:18:17 +0200

    Add persistence

--- Runtime/Scripts/AnimatorDriver.cs
+++ Runtime/Scripts/AnimatorDriver.cs
@@ -1,34 +1,64 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
 
 namespace UwUtils
 {
     [AddComponentMenu("UwUtils/AnimatorDriver")]
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class AnimatorDriver : UdonSharpBehaviour
     {
         [SerializeField] private Animator Animator;
         [SerializeField] private string ParameterName;
         [SerializeField] private bool defaultValue;
+        [Header("Save state with persistence?")]
+        [SerializeField] private bool saveState = false;
+        [Header("Use a unique Parameter per toggle!")]
+        [SerializeField] private string persistenceParameter = "toggle_example";
         private bool abort;
 
         public void Start()
         {
-            if (Animator == null | ParameterName == null)
+            if (Animator == null || ParameterName == null)
             {
                 abort = true;
-                SendCustomEventDelayedSeconds(nameof(Animator), 1f);
+                SendCustomEventDelayedSeconds(nameof(_sendDebugError), 1f);
                 return;
             }
             Animator.SetBool(ParameterName, defaultValue);
+            if (saveState) PlayerData.SetBool(persistenceParameter, defaultValue);
         }
 
         public override void Interact()
         {
             if (abort) return;
-            Animator.SetBool(ParameterName, !defaultValue);
+            defaultValue = !defaultValue;
+            Animator.SetBool(ParameterName, defaultValue);
+            if (saveState) PlayerData.SetBool(persistenceParameter, defaultValue);
         }
 
-        public void _sendDebugError() => Debug.LogError("Reava_UwUtils:<color=red> <b>Invalid references</b></color>, please review <color=orange>Animator reference</color> / <color=orange>Parameter name</color>. (" + gameObject + ")", gameObject);
+        public void _Toggle()
+        {
+            if (abort) return;
+            defaultValue = !defaultValue;
+            Animator.SetBool(ParameterName, defaultValue);
+            if (saveState) PlayerData.SetBool(persistenceParameter, defaultValue);
+        }
+
+        public override void OnPlayerRestored(VRCPlayerApi player)
+        {
+            if (!abort && saveState)
+            {
+                if (Networking.LocalPlayer != player) return;
+                if (!PlayerData.HasKey(player, persistenceParameter)) return;
+                if (PlayerData.GetType(player, persistenceParameter) != typeof(bool)) return;
+
+                bool recoveredState = PlayerData.GetBool(player, persistenceParameter);
+                if (!recoveredState) _Toggle();
+            }
+        }
+
+        public void _sendDebugError() => Debug.LogError("[UwUtils/AnimatorDriver.cs] Invalid references, please review Animator reference / Parameter name on: '" + gameObject + "'", gameObject);
     }
 }
--- Runtime/Scripts/ObjectsToggle.cs
+++ Runtime/Scripts/ObjectsToggle.cs
@@ -1,18 +1,27 @@
 ﻿using UnityEngine;
 using UdonSharp;
+using UnityEngine.InputSystem;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
 
 namespace UwUtils
 {
     [AddComponentMenu("UwUtils/Objects Toggle")]
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class ObjectsToggle : UdonSharpBehaviour
     {
         [Header("List of objects to toggle")]
         [SerializeField] private GameObject[] toggleObjects;
+        [Header("Save state with persistence? Warning, this will NOT save exact states, read more by hovering.")]
+        [Tooltip("If you have another toggle than this one setting the state of one of the objects differently, this will NOT know it and won't restore it properly when rejoining. For accuracy, only use one toggle per object, and separate anything that needs to be saved differently from the rest of the group.")]
+        [SerializeField] private bool saveState = false;
+        [Header("Use a unique Parameter per toggle!")]
+        [SerializeField] private string persistenceParameter = "toggle_example";
+        private bool isDefault = true;
         private bool valid = true;
         void Start()
         {
-            if (toggleObjects.Length == 0)
+            if (toggleObjects.Length == 0 || toggleObjects == null)
             {
                 valid = false;
                 Debug.LogError("[UwUtils/iState.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
@@ -25,32 +34,40 @@ namespace UwUtils
 
         public override void Interact()
         {
-            foreach (GameObject toggleObject in toggleObjects)
+            if (valid)
             {
-                toggleObject.SetActive(!toggleObject.activeSelf);
+                foreach (GameObject toggleObject in toggleObjects)
+                {
+                    if (toggleObject) toggleObject.SetActive(!toggleObject.activeSelf);
+                }
+                isDefault = !isDefault;
+                if (saveState) PlayerData.SetBool(persistenceParameter, isDefault);
             }
         }
 
-        public void _EnableAll()
+        public void _Toggle()
         {
-            foreach (GameObject toggleObject in toggleObjects)
+            if (valid)
             {
-                toggleObject.SetActive(true);
-            }
-        }
-        public void _DisableAll()
-        {
-            foreach (GameObject toggleObject in toggleObjects)
-            {
-                toggleObject.SetActive(false);
+                foreach (GameObject toggleObject in toggleObjects)
+                {
+                    if (toggleObject) toggleObject.SetActive(!toggleObject.activeSelf);
+                }
+                isDefault = !isDefault;
+                if (saveState) PlayerData.SetBool(persistenceParameter, isDefault);
             }
         }
 
-        public void _InvertState()
+        public override void OnPlayerRestored(VRCPlayerApi player)
         {
-            foreach (GameObject toggleObject in toggleObjects)
+            if (valid && saveState)
             {
-                toggleObject.SetActive(!toggleObject.activeSelf);
+                if (Networking.LocalPlayer != player) return;
+                if (!PlayerData.HasKey(player, persistenceParameter)) return;
+                if (PlayerData.GetType(player, persistenceParameter) != typeof(bool)) return;
+
+                bool recoveredState = PlayerData.GetBool(player, persistenceParameter);
+                if (!recoveredState) _Toggle();
             }
         }
     }
