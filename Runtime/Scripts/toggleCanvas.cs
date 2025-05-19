3bc34ec17b5ac694247dff98dda505bb02f07600
Reava <reava.vr@gmail.com>
2025-05-19 08:11:18 +0200

    Add Persistence to: toggleCanvas.cs

--- Runtime/Scripts/toggleCanvas.cs
+++ Runtime/Scripts/toggleCanvas.cs
@@ -1,7 +1,8 @@
 ﻿using UdonSharp;
-using Unity.Collections;
 using UnityEngine;
-using VRC.SDK3.Components;
+using UnityEngine.UI;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
 
 namespace UwUtils
 {
@@ -9,8 +10,28 @@ namespace UwUtils
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class toggleCanvas : UdonSharpBehaviour
     {
+        [SerializeField] private bool defaultState = true;
         [Header("List of objects")]
         public Canvas[] canvasesToToggle;
+        public Canvas[] canvasesToToggleinverted;
+
+        [SerializeField] private bool usePersistence = false;
+        [SerializeField] private string PersistenceKey = "Reava_UwUtils_Toggle_Canvas_";
+
+        private bool currentState;
+
+        public void Start()
+        {
+            foreach (Canvas toggleObject in canvasesToToggle)
+            {
+                toggleObject.enabled = defaultState;
+            }
+            foreach (Canvas toggleObject in canvasesToToggleinverted)
+            {
+                toggleObject.enabled = !defaultState;
+            }
+            currentState = defaultState;
+        }
 
         public override void Interact()
         {
@@ -18,6 +39,12 @@ namespace UwUtils
             {
                 toggleObject.enabled = !toggleObject.enabled;
             }
+            foreach (Canvas toggleObject in canvasesToToggleinverted)
+            {
+                toggleObject.enabled = !toggleObject.enabled;
+            }
+            currentState = !currentState;
+            if (usePersistence) PlayerData.SetBool(PersistenceKey, currentState);
         }
 
         public void _Enable()
@@ -26,6 +53,12 @@ namespace UwUtils
             {
                 toggleObject.enabled = true;
             }
+            foreach (Canvas toggleObject in canvasesToToggleinverted)
+            {
+                toggleObject.enabled = false;
+            }
+            currentState = true;
+            if (usePersistence) PlayerData.SetBool(PersistenceKey, currentState);
         }
 
         public void _Disable()
@@ -33,6 +66,32 @@ namespace UwUtils
             foreach (Canvas toggleObject in canvasesToToggle)
             {
                 toggleObject.enabled = false;
+            }
+            foreach (Canvas toggleObject in canvasesToToggleinverted)
+            {
+                toggleObject.enabled = true;
+            }
+            currentState = false;
+            if (usePersistence) PlayerData.SetBool(PersistenceKey, currentState);
+        }
+
+        public override void OnPlayerRestored(VRCPlayerApi player)
+        {
+            if (usePersistence)
+            {
+                if (Networking.LocalPlayer != player) return;
+                if (!PlayerData.HasKey(player, PersistenceKey)) return;
+                if (PlayerData.GetType(player, PersistenceKey) != typeof(bool)) return;
+
+                bool recoveredState = PlayerData.GetBool(player, PersistenceKey);
+                if (recoveredState)
+                {
+                    _Enable();
+                }
+                else
+                {
+                    _Disable();
+                }
             }
         }
     }
