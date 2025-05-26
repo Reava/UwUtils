0953cbf9c7e307aac353b9774b668b019fc46eac
Reava <reava.vr@gmail.com>
2025-05-26 14:37:32 +0200

    Update DailyCheckin.cs
    Remove daily rejoin relay (just use OnJoin script), added function check return for checking if it is the user's first join of the day for other scripts, and fixes.

--- Runtime/Scripts/DailyCheckin.cs
+++ Runtime/Scripts/DailyCheckin.cs
@@ -16,11 +16,13 @@ namespace UwUtils
         [SerializeField] private const string scorePersistenceParameterDaily = "Reava_UwUtils_Scores_Daily";
 
         // There is current no check for actual streaks.
+        [SerializeField] private bool trackDayCount = true;
         [SerializeField] private string DisplayPrefix = "Days joined: ";
         [SerializeField] private TextMeshProUGUI currentStreak;
         [SerializeField] private string eventName = "_interact";
+        [Header("Events on first join of the day")]
         [SerializeField] private UdonBehaviour[] TriggerOnFirstJoin;
-        [SerializeField] private UdonBehaviour[] TriggerOnDailyReward;
+        [SerializeField] private GameObject[] toggleOnFirstJoin;
         [SerializeField] private bool alsoTriggerOnSameDayRejoin = true;
 
         [SerializeField] private int rewardDaily = 1;
@@ -34,10 +36,17 @@ namespace UwUtils
 
         private bool _playerRestored = false;
 
+        [HideInInspector] public bool wasFirstDailyJoin = false;
+
         void Start()
         {
             todayDate = System.DateTime.Now.ToString(dateFormat);
             SendCustomEventDelayedSeconds(nameof(_checkRestored), 5f);
+        }
+
+        public bool _isFirstDailyJoin()
+        {
+            return wasFirstDailyJoin;
         }
 
         public void _dailyFirstJoin()
@@ -45,28 +54,29 @@ namespace UwUtils
             currentValue = PlayerData.GetInt(Networking.LocalPlayer, scorePersistenceParameterDaily);
             currentValue += 1;
             PlayerData.SetInt(scorePersistenceParameterDaily, currentValue);
+            foreach (UdonBehaviour eTarget in TriggerOnFirstJoin)
+            {
+                if (eTarget) eTarget.SendCustomEvent(eventName);
+            }
+            foreach (GameObject o in toggleOnFirstJoin)
+            {
+                if (o) o.SetActive(!o.activeSelf);
+            }
             _dailyRejoin();
         }
 
         public void _dailyRejoin()
         {
             if (currentStreak) currentStreak.text = DisplayPrefix + currentValue.ToString();
-            foreach (UdonBehaviour eTarget in TriggerOnDailyReward)
-            {
-                if (eTarget) eTarget.SendCustomEvent(eventName);
-            }
         }
 
         public void _checkRestored()
         {
             if (!_playerRestored)
             {
-                PlayerData.SetInt(scorePersistenceParameterDaily, 1);
-                foreach (UdonBehaviour eTarget in TriggerOnFirstJoin)
-                {
-                    if (eTarget) eTarget.SendCustomEvent(eventName);
-                }
                 _dailyRejoin();
+                if (!trackDayCount) return;
+                PlayerData.SetInt(scorePersistenceParameterDaily, 1);
             }
         }
 
@@ -78,7 +88,7 @@ namespace UwUtils
             if (!PlayerData.HasKey(player, LastJoinDateKey)) return;
             if (PlayerData.GetType(player, LastJoinDateKey) != typeof(string)) return;
             lastJoinDate = PlayerData.GetString(Networking.LocalPlayer, LastJoinDateKey);
-            if (lastJoinDate != todayDate) _dailyFirstJoin();
+            if(alsoTriggerOnSameDayRejoin || lastJoinDate != todayDate) _dailyFirstJoin(); wasFirstDailyJoin = true;
             if (lastJoinDate == todayDate) _dailyRejoin();
         }
     }
