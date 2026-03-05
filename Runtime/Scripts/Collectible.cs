446eb478b8ec82f0f524ac525a3ea443a2e6301c
Reava <reava.vr@gmail.com>
2026-03-05 18:29:35 +0100

    Collectible system V3 + Drag n' Drop Prefab

--- Runtime/Scripts/Collectible.cs
+++ Runtime/Scripts/Collectible.cs
@@ -11,40 +11,58 @@ namespace UwUtils
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class Collectible : UdonSharpBehaviour
     {
+        [SerializeField] private int Value = 1;
+        [SerializeField] private CollectionSystem CollectionSystemRef;
         [Space]
         [SerializeField] private bool disableSelfOnCollect = true;
-        [SerializeField] private CollectionSystem CollectionSystemRef;
         [Header("Objects to enable/disable on collectible collection")]
         [SerializeField] private GameObject[] EnableOnCollect;
         [SerializeField] private GameObject[] DisableOnCollect;
         [Header("Behaviors to relay events to when an object is collected")]
-        [SerializeField] string EventName = "_interact";
+        [SerializeField] private string[] EventNames;
         [SerializeField] private UdonBehaviour[] EventRelays;
-        [SerializeField] private int Value = 10;
-        
+        [Space]
         [Header("Keep collectible state after rejoin ?"), Tooltip("Warning, if you enable it here but not on the System manager it will NOT save the points collected. If disabled here but enabled on the system, this will allow the user to collect them again on rejoin for an accumulation of points.")]
         [SerializeField] private bool persistent = false;
         [Tooltip("Relay events to listed Udon Behaviors when the recovered state from persistence is collected, this helps avoid particle systems and other similar 'on collect' events from happening on join.")]
         [SerializeField] private bool relayOnRecovered = true;
-        [Header("Unique persistence ID"),Tooltip("Make sure you use a unique parameter.")]
+        [Header("Unique persistence ID"), Tooltip("Make sure you use a unique parameter.")]
         [SerializeField] private string persistenceParameter = "UwUtils_Collectible_UNIQUEID";
-        
+        [Tooltip("Warning: Changing the object name with this option on WILL BREAK PERSISTENCE")]
+        [SerializeField] private bool UseObjectNameAsPersistenceKey = true;
         [Space]
-        [Header("Per collectible debug settings"),Tooltip("If logging is disable, no support will be given.")]
+        [Header("Per collectible debug settings"), Tooltip("If logging is disable, no support will be given.")]
         [SerializeField] private bool enableLogging = true;
         [SerializeField] private bool StartupLogging = false;
         [HideInInspector] public bool collected = false;
 
         void Start()
         {
-            if(persistenceParameter == "UwUtils_Collectible_UNIQUEID" && persistent)
+            if ((persistenceParameter == "UwUtils_Collectible_UNIQUEID" || persistenceParameter == "UwUtils_Collectible_") && persistent)
             {
-                Debug.LogError("[Reava_/UwUtils/Collectible.cs]: Collectible does NOT use a custom Persistence parameter, this will break saving! Please update collectible named: "+this.name,this.gameObject);
-                // do something about the user not following instructions to avoid persistence conflicts
-                // >> EDITOR SCRIPT https://docs.unity3d.com/ScriptReference/GlobalObjectId.html 
+                if (UseObjectNameAsPersistenceKey)
+                {
+                    persistenceParameter += this.gameObject.name;
+                }
+                else
+                {
+                    Debug.LogError("[Reava_/UwUtils/Collectible.cs]: Collectible does NOT use a custom Persistence parameter, this will break saving! Please update collectible named: " + this.name, this.gameObject);
+                    // do something about the user not following instructions to avoid persistence conflicts
+                    // >> EDITOR SCRIPT https://docs.unity3d.com/ScriptReference/GlobalObjectId.html
+                }
             }
-            CollectionSystemRef._totalValueDebug(Value);
-            if (enableLogging && StartupLogging) _debugLog("initialized");
+            SendCustomEventDelayedSeconds(nameof(_collectTotalTally), 0.1f);
+
+            if (enableLogging && StartupLogging) _debugLog($"initialized object  {this.name}");
+
+            foreach (GameObject o in EnableOnCollect)
+            {
+                if (o) o.SetActive(false);
+            }
+            foreach (GameObject o in DisableOnCollect)
+            {
+                if (o) o.SetActive(true);
+            }
         }
 
         public override void Interact()
@@ -52,12 +70,18 @@ namespace UwUtils
             _Collect();
         }
 
+        public void _collectTotalTally()
+        {
+            CollectionSystemRef._totalValueDebug(Value);
+            if (enableLogging && StartupLogging) _debugLog("tally for " + this.gameObject.name);
+        }
+
         public void _Collect()
         {
             CollectionSystemRef._collectValue(Value);
             if (enableLogging) _debugLog("claimed");
             collected = true;
-            if(persistent) PlayerData.SetBool(persistenceParameter, true);
+            if (persistent) PlayerData.SetBool(persistenceParameter, true);
             _PostCollection(false);
         }
 
@@ -73,9 +97,9 @@ namespace UwUtils
                 if (o) o.SetActive(false);
             }
             if (!relayOnRecovered && recovered) return;
-            foreach (UdonBehaviour o in EventRelays)
+            for(int i = 0; i < EventRelays.Length && i < EventNames.Length; i++)
             {
-                if (o) o.SendCustomEvent(EventName);
+                if (EventRelays[i] != null) EventRelays[i].SendCustomEvent(EventNames[i]);
             }
         }
 
@@ -90,6 +114,8 @@ namespace UwUtils
                 bool recoveredState = PlayerData.GetBool(player, persistenceParameter);
                 if (enableLogging) _debugLog("recovered persistence state of : " + recoveredState + "");
                 if (recoveredState) _PostCollection(true);
+
+                CollectionSystemRef._collectValue(Value);
             }
         }
 
--- Runtime/Scripts/CollectionSystem.cs
+++ Runtime/Scripts/CollectionSystem.cs
@@ -1,9 +1,7 @@
-﻿using UnityEngine;
+﻿using TMPro;
 using UdonSharp;
-using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
-using VRC.SDK3.Persistence;
-using VRC.SDKBase;
 using VRC.Udon;
 
 namespace UwUtils
@@ -13,30 +11,30 @@ namespace UwUtils
     public class CollectionSystem : UdonSharpBehaviour
     {
         [Space]
-        [Tooltip("Default balance. Making this above 0 with persistence on will act as a join reward, adding to the user's balance every time they join the world!")]
+        [Tooltip("Default balance")]
         [SerializeField] private int balance = 0;
         [Space]
         [SerializeField] private bool outputToText = true;
+        [SerializeField] private bool ShowMax = true;
         [SerializeField] private string textPrefix = "Balance: ";
-        [SerializeField] private GameObject[] textFieldsBalance;
-        [SerializeField] private GameObject[] textFieldsTotal;
-        [SerializeField] private GameObject[] textFieldsRemaining;
+        [SerializeField] private Text[] textFieldsBalanceUnity;
+        [SerializeField] private TextMeshPro[] textFieldsBalanceTMPMesh;
+        [SerializeField] private TextMeshProUGUI[] textFieldsBalanceTMPUI;
         [Space]
         [Header("Unlockables Section. ARRAYS LENGTHS MUST MATCH")]
         [SerializeField] private bool enableRewardTiers = false;
+        [Tooltip("Please use incremental values. (Example: 5,10,25)")]
         [SerializeField] private int[] tierValues;
         [Tooltip("Upon Unlocking a tier, toggles objects matching of the tier.")]
         [SerializeField] private GameObject[] unlockableTierObjects;
-        [Tooltip("Upon Recovered, will only trigger the highest tier unlocked!")]
-        [SerializeField] private bool relayOnRecovered = true;
         [Tooltip("Upon Unlocking a tier, sends an event to those behaviors. Reference a relay to send a single tier event to multiple scripts!")]
         [SerializeField] private UdonBehaviour[] relayPerTier;
-        [SerializeField] private string eventName = "_interact";
-        [Space]
-        [Header("Keep balance after rejoin ?")]
-        [SerializeField] private bool persistent = false;
-        [Header("Unique persistence IDs"), Tooltip("Make sure you use a unique parameter.")]
-        [SerializeField] private string persistenceParameter = "UwUtils_CollectibleSystem_Balance";
+        [SerializeField] private string[] eventNames;
+        [Header("Completion Rewards")]
+        [SerializeField] private UdonBehaviour[] CompletionRelays;
+        [SerializeField] private string[] CompletionEventNames;
+        [SerializeField] private GameObject[] CompletionRewardsOn;
+        [SerializeField] private GameObject[] CompletionRewardsOff;
         [Space]
         [Tooltip("If logging is disabled, no support will be given.")]
         [SerializeField] private bool enableLogging = true;
@@ -45,79 +43,88 @@ namespace UwUtils
 
         void Start()
         {
-            if (outputToText && textFieldsBalance == null) { outputToText = false; Debug.LogError("[Reava_/UwUtils/CollectionSystem.cs]: Ouput to text enabled but no text fields found, disabling feature on " + gameObject.name, gameObject); }
-            debugTotalValue = 0;
-            if(tierValues.Length > 0)
-            {
-                // Array.Sort(tierValues); // I would love to use it if VRChat allowed us to so users don't use random order :))
-                // Array.Reverse(tierValues);
+            if (outputToText && (textFieldsBalanceUnity.Length == 0 && textFieldsBalanceTMPUI.Length == 0 && textFieldsBalanceTMPMesh.Length == 0)) {
+                outputToText = false; Debug.LogError("[Reava_/UwUtils/CollectionSystem.cs]: Ouput to text enabled but no text fields found, disabling feature on " + gameObject.name, gameObject);
             }
+
+            debugTotalValue = 0 + balance;
+
             if (enableLogging) SendCustomEventDelayedSeconds(nameof(_maxValueDebug), 5f);
         }
 
         public void _totalValueDebug(int valuePerCollectible)
         {
             debugTotalValue += valuePerCollectible;
+            if (enableLogging) _debugLog("Received new collectible worth " + valuePerCollectible + "! New max is: " + debugTotalValue);
+            _printValue();
         }
 
         public void _collectValue(int CollectibleValue)
         {
+            if (balance >= debugTotalValue)
+            {
+                if (enableLogging) _debugLog("Balance is already " + balance + "! Maximum is: " + debugTotalValue + ". Please make sure collectible disables itself on collection.");
+            }
+
             if (enableLogging) _debugLog("Received new collectible worth " + CollectibleValue + "! New balance is: " + balance);
             balance += CollectibleValue;
-            
+            _updateBalance();
         }
 
         public void _updateBalance()
         {
             if (outputToText) _printValue();
-            if (persistent) PlayerData.SetInt(persistenceParameter, balance);
+
+            _CompletionConfirmation(balance);
+
             if (!enableRewardTiers) return;
             for (int i = 0; i < tierValues.Length; i++)
             {
-                if(balance >= tierValues[i])
+                if (balance >= tierValues[i])
                 {
                     if (unlockableTierObjects[i]) unlockableTierObjects[i].SetActive(!unlockableTierObjects[i].activeSelf);
-                    if (relayPerTier[i]) relayPerTier[i].SendCustomEvent(eventName);
+                    if (relayPerTier[i])
+                    {
+                        relayPerTier[i].SendCustomEvent(eventNames[i]);
+                    }
                 }
             }
         }
 
-        public void _printValue()
+        public void _CompletionConfirmation(int balance)
         {
-            foreach (GameObject field in textFieldsBalance)
+            if (balance < debugTotalValue) return;
+
+            foreach (GameObject o in CompletionRewardsOn)
             {
-                if (!field) continue;
-                if (field.GetComponent<TextMeshProUGUI>() != null) { field.GetComponent<TextMeshProUGUI>().text = textPrefix + balance; continue; }
-                if (field.GetComponent<TextMeshPro>() != null) { field.GetComponent<TextMeshPro>().text = textPrefix + balance; continue; }
-                if (field.GetComponent<Text>() != null) { field.GetComponent<Text>().text = textPrefix + balance; continue; }
+                if (o) o.SetActive(true);
             }
-            foreach (GameObject field in textFieldsTotal)
+            foreach (GameObject o in CompletionRewardsOff)
             {
-                if (!field) continue;
-                if (field.GetComponent<TextMeshProUGUI>() != null) { field.GetComponent<TextMeshProUGUI>().text = "" + debugTotalValue; continue; }
-                if (field.GetComponent<TextMeshPro>() != null) { field.GetComponent<TextMeshPro>().text = "" + debugTotalValue; continue; }
-                if (field.GetComponent<Text>() != null) { field.GetComponent<Text>().text = "" + debugTotalValue; continue; }
+                if (o) o.SetActive(false);
             }
-            foreach (GameObject field in textFieldsRemaining)
+            for (int i = 0; i < CompletionRelays.Length && i < CompletionEventNames.Length; i++)
             {
-                if (!field) continue;
-                if (field.GetComponent<TextMeshProUGUI>() != null) { field.GetComponent<TextMeshProUGUI>().text = ""+(debugTotalValue - balance); continue; }
-                if (field.GetComponent<TextMeshPro>() != null) { field.GetComponent<TextMeshPro>().text = "" + (debugTotalValue - balance); continue; }
-                if (field.GetComponent<Text>() != null) { field.GetComponent<Text>().text = "" + (debugTotalValue - balance); continue; }
+                if (CompletionRelays[i] != null) CompletionRelays[i].SendCustomEvent(CompletionEventNames[i]);
             }
+
+            _debugLog("Completed collection confirmed!");
         }
 
-        public override void OnPlayerRestored(VRCPlayerApi player)
+        public void _printValue()
         {
-            if (persistent)
+
+            foreach (Text field in textFieldsBalanceUnity)
             {
-                if (Networking.LocalPlayer != player) return;
-                if (!PlayerData.HasKey(player, persistenceParameter)) return;
-                if (PlayerData.GetType(player, persistenceParameter) != typeof(int)) return;
-                int recoveredBalance = PlayerData.GetInt(player, persistenceParameter);
-                if (enableLogging) _debugLog("Recovered balance of : " + recoveredBalance + "");
-                if (recoveredBalance > 0) balance = recoveredBalance;
-                if (outputToText) _printValue();
+                if (field != null) { field.text = ShowMax ? textPrefix + balance + "/" + debugTotalValue : textPrefix + balance; continue; }
+            }
+            foreach (TextMeshPro field in textFieldsBalanceTMPMesh)
+            {
+                if (field != null) { field.text = ShowMax ? textPrefix + balance + "/" + debugTotalValue : textPrefix + balance; continue; }
+            }
+            foreach (TextMeshProUGUI field in textFieldsBalanceTMPUI)
+            {
+                if (field != null) { field.text = ShowMax ? textPrefix + balance + "/" + debugTotalValue : textPrefix + balance; continue; }
             }
         }
 
