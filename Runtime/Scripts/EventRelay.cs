004ed67d74b31301480cbb97b9e6aa36a750f70b
Reava <reava.vr@gmail.com>
2026-03-06 13:46:08 +0100

    Simplify script and change defaults

--- Runtime/Scripts/EventRelay.cs
+++ Runtime/Scripts/EventRelay.cs
@@ -8,16 +8,11 @@ namespace UwUtils
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class EventRelay : UdonSharpBehaviour
     {
-        [Header("Send an event call to any behavior with conditions and/or delay")]
+        [Header("Send an event call to any behavior with delay")]
         [SerializeField] private UdonBehaviour[] programRelay;
-        [SerializeField] private GameObject stateCheck;
-        private bool stateChecked;
         [SerializeField] private string[] eventNames;
-        [SerializeField] private bool delayedAction;
-        [SerializeField] private float delay;
-        [Header("0 = Keep delay, 1 = No delay when object is off, 2 = No delay when object is on")]
-        [Range(0, 2)]
-        [SerializeField] private int function = 1;
+        [SerializeField] private bool delayAction = false;
+        [SerializeField] private float delay = 0f;
         private bool abort = false;
 
         public void Start()
@@ -25,55 +20,20 @@ namespace UwUtils
             if (programRelay.Length == 0)
             {
                 abort = true;
-                SendCustomEventDelayedSeconds(nameof(_sendDebugError), 1f);
+                SendCustomEventDelayedSeconds(nameof(_sendDebugError), 0.1f);
                 return;
             }
         }
         public override void Interact()
         {
             if (abort) return;
-            if (!delayedAction)
+            if (!delayAction)
             {
                 _relayAction();
             }
             else
             {
-                if (stateCheck == null)
-                {
-                    Debug.LogWarning("[Reava_/UwUtils/EventRelay.cs] No State check object detected for delayed action, please assign it and refer to doc", this);
-                    stateChecked = true;
-                }
-                else
-                {
-                    stateChecked = stateCheck.activeSelf;
-                }
-
-                if (function == 0)
-                {
-                    SendCustomEventDelayedSeconds(nameof(_relayAction), delay);
-                }
-                else if (function == 1)
-                {
-                    if (stateChecked)
-                    {
-                        SendCustomEventDelayedSeconds(nameof(_relayAction), delay);
-                    }
-                    else
-                    {
-                        _relayAction();
-                    }
-                }
-                else if (function == 2)
-                {
-                    if (!stateChecked)
-                    {
-                        SendCustomEventDelayedSeconds(nameof(_relayAction), delay);
-                    }
-                    else
-                    {
-                        _relayAction();
-                    }
-                }
+                SendCustomEventDelayedSeconds(nameof(_relayAction), delay);
             }
         }
 
