b234ee05bf2bee5f8627f7e6bf13229dd5aa99b6
Reava <reava.vr@gmail.com>
2026-03-06 13:51:35 +0100

    New Script: InteractionTextSetter
    Allows switching the interaction text of one or more udonbehaviors on interact / pickup use down

---
+++ Runtime/Scripts/InteractionTextSetter.cs
@@ -0,0 +1,73 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Udon;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Interaction Text Setter")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class InteractionTextSetter : UdonSharpBehaviour
+    {
+        [Header("Targets")]
+        public UdonBehaviour[] targetBehaviours;
+
+        [Header("Text")]
+        public string defaultText = "Click Me!";
+        public string setText = "Undo";
+
+        [Space]
+        public bool setTextOnStart = true;
+
+        private bool _isSet = false;
+
+        void Start()
+        {
+            if (!setTextOnStart) return;
+            _ApplyText(defaultText);
+            _isSet = false;
+        }
+
+        public void _SetText()
+        {
+            _ApplyText(setText);
+            _isSet = true;
+        }
+
+        public void _ResetText()
+        {
+            _ApplyText(defaultText);
+            _isSet = false;
+        }
+
+        public override void Interact()
+        {
+            _ToggleText();
+        }
+
+        public override void OnPickupUseDown()
+        {
+            Interact();
+        }
+
+        public void _ToggleText()
+        {
+            if (_isSet)
+            {
+                _ResetText();
+            }
+            else
+            {
+                _SetText();
+            }
+        }
+
+        private void _ApplyText(string text)
+        {
+            for (int i = 0; i < targetBehaviours.Length; i++)
+            {
+                UdonBehaviour ub = targetBehaviours[i];
+                if (ub != null) ub.InteractionText = text;
+            }
+        }
+    }
+}
