d4273e264992efee762bd36374e5ac1aa5ec1979
Reava <reava.vr@gmail.com>
2026-03-13 05:44:38 +0100

    New Script: OpenWorldStore

---
+++ Runtime/Scripts/OpenWorldStore.cs
@@ -0,0 +1,27 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Economy;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Open World Store")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OpenWorldStore : UdonSharpBehaviour
+    {
+
+        public override void Interact()
+        {
+            Store.OpenWorldStorePage();
+        }
+
+        public override void OnPickupUseDown()
+        {
+            Store.OpenWorldStorePage();
+        }
+
+        public void _OpenStore()
+        {
+            Store.OpenWorldStorePage();
+        }
+    }
+}
