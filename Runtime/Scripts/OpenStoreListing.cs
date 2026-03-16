86b4366f03d8e9f1721b652957ac56b89caad23d
Reava <reava.vr@gmail.com>
2026-03-16 16:08:12 +0100

    Add CE Store Listing Open Script

---
+++ Runtime/Scripts/OpenStoreListing.cs
@@ -0,0 +1,35 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Economy;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Open Store Listing")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OpenStoreListing : UdonSharpBehaviour
+    {
+        public string listingID = "";
+
+        public void _OpenListing()
+        {
+            Store.OpenListing(listingID);
+        }
+
+        public override void Interact()
+        {
+            Store.OpenListing(listingID);
+        }
+
+        public override void OnPickupUseDown()
+        {
+            Store.OpenListing(listingID);
+        }
+
+        public void _OpenStore()
+        {
+            Store.OpenListing(listingID);
+        }
+    }
+}
