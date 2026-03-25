5ddf192caa9ec85d8f935fc05997c1034006ca50
Reava <reava.vr@gmail.com>
2026-03-25 17:23:19 +0100

    New script: Local User Name Display

---
+++ Runtime/Scripts/LocalUserDisplaynameDisplay.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Collection System")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LocalUserDisplaynameDisplay : UdonSharpBehaviour
+    {
+        public TextMeshProUGUI textMeshProUGUI;
+        void Start()
+        {
+            string playerUsername = Networking.LocalPlayer.displayName;
+            textMeshProUGUI.text = playerUsername;
+        }
+    }
+}
