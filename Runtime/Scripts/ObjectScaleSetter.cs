398dae8d288bb350f0d4d6f8161befaaff63f3f8
Reava <reava.vr@gmail.com>
2026-04-27 23:26:02 +0200

    New Script: Object Scale Setter
    This script allows you to scale objects on demand or on start (delay supported)

---
+++ Runtime/Scripts/ObjectScaleSetter.cs
@@ -0,0 +1,47 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Udon;
+
+namespace UwUtils
+{
+    public class ObjectScaleSetter : UdonSharpBehaviour
+    {
+        [Header("Target to scale")]
+        public Transform[] referenceTransforms;
+
+        [Header("Scale to apply (MUST MATCH TRANSFORM ARRAY LENGTH)")]
+        public Vector3[] targetScales;
+
+        public float delay = 1f;
+        public bool applyOnStart = true;
+
+        void Start()
+        {
+            if (applyOnStart) SendCustomEventDelayedSeconds(nameof(_ApplyScale), delay);
+        }
+
+        public void _ApplyScale()
+        {
+            if (referenceTransforms.Length == 0)
+            {
+                Debug.LogWarning("[Reava_/UwUtils/ObjectScaleSetter.cs]: Reference Transform is not assigned.");
+                return;
+            }
+
+            int max = Mathf.Min(referenceTransforms.Length, targetScales.Length);
+            for (int i = 0; i < max; i++)
+            {
+                referenceTransforms[i].localScale = targetScales[i];
+            }
+        }
+        private void OnDisable()
+        {
+            _ApplyScale();
+        }
+
+        private void OnEnable()
+        {
+            _ApplyScale();
+        }
+    }
+}
