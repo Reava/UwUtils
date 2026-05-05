213b74bf74511a4c7ec2c7c0d0dfe1b8503bd3b4
Reava <reava.vr@gmail.com>
2026-05-05 21:23:27 +0200

    New Script: PlayOneshotAudio

---
+++ Runtime/Scripts/PlayOneshotAudio.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Play Oneshot Audio")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class PlayOneshotAudio : UdonSharpBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private AudioSource AudioSource;
+
+        public void _PlayAudio()
+        {
+            if (!AudioSource)
+            {
+                Debug.LogWarning($"[Reava_/UwUtils/PlayOneshotAudio.cs] {nameof(AudioSource)} is not set");
+                return;
+            }
+
+            AudioSource.time = 0;
+            AudioSource.PlayOneShot(AudioSource.clip);
+        }
+    }
+}
