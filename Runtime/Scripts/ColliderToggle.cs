d29f16cbb8a5a2a9df0df4fd4b295dabd61623bf
Reava <reava.vr@gmail.com>
2026-05-15 14:19:21 +0200

    v1.0.0-pre.23: +2 Scripts & CE Folder
    v1.0.0-pre.23: +2 Scripts & CE Folder
    # Added Scripts:
    - ColliderToggle: Enables toggling colliders, supports _Toggle, _SetOn, and _SetOff
    - Creator Economy Product Relay: Event Relay for CE Product Ownership
    - Moved CE Scripts to their own folder.

---
+++ Runtime/Scripts/ColliderToggle.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UdonSharp;
+using UnityEngine.InputSystem;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/Collider Toggle")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ColliderToggle : UdonSharpBehaviour
+    {
+        [Header("List of colliders to toggle")]
+        [SerializeField] private Collider[] toggleCols;
+        public bool setDefaultOnStart = false;
+        public bool defaultState = true;
+
+        private bool isDefault = true;
+
+        void Start()
+        {
+            if (defaultState)
+            {
+                _SetOn();
+            }
+            else
+            {
+                _SetOff();
+            }
+        }
+
+        public override void Interact()
+        {
+            _Toggle();
+        }
+        public override void OnPickupUseDown()
+        {
+            Interact();
+        }
+
+        public void _Toggle()
+        {
+            foreach (Collider col in toggleCols)
+            {
+                col.enabled = !col.enabled;
+            }
+            isDefault = !isDefault;
+        }
+        public void _SetOn()
+        {
+            foreach (Collider col in toggleCols)
+            {
+                col.enabled = true;
+            }
+            isDefault = !isDefault;
+        }
+        public void _SetOff()
+        {
+            foreach (Collider col in toggleCols)
+            {
+                col.enabled = false;
+            }
+            isDefault = !isDefault;
+        }
+    }
+}
---
+++ Runtime/Scripts/Creator Economy/CreatorEconomyProductRelay.cs
@@ -0,0 +1,75 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Economy;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UwUtils
+{
+    [AddComponentMenu("UwUtils/CE Product Relay")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class CreatorEconomyProductRelay : UdonSharpBehaviour
+    {
+        [Header("Settings")]
+        [Tooltip("Only enable this for testing")]
+        [SerializeField] public bool isOwned = false;
+        [SerializeField] private UdonProduct product;
+
+        [Header("Purchased Product Event Relays")]
+        [Tooltip("ProgramsRelays and EventNames MUST match in array sizes!")]
+        [SerializeField] private UdonBehaviour[] PurchasedProgramRelays;
+        [Tooltip("ProgramsRelays and EventNames MUST match in array sizes!")]
+        [SerializeField] private string[] PurchasedEventNames;
+        [Header("Expired Product Event Relays")]
+        [Tooltip("If enabled, this will send out experied events on start")]
+        [SerializeField] private bool relayExpiredOnStart = false;
+        [Tooltip("ProgramsRelays and EventNames MUST match in array sizes!")]
+        [SerializeField] private UdonBehaviour[] ExpiredProgramRelays;
+        [Tooltip("ProgramsRelays and EventNames MUST match in array sizes!")]
+        [SerializeField] private string[] ExpiredEventNames;
+
+        private void Start()
+        {
+            if(!Utilities.IsValid(product))
+            {
+                this.enabled = false;
+                Debug.LogError("[Reava_/UwUtils/CreatorEconomyProductRelay.cs] No UdonProduct Specified, disabling self", gameObject);
+                return;
+            }
+
+            if (relayExpiredOnStart)
+            {
+                for (int i = 0; i < ExpiredProgramRelays.Length; i++)
+                {
+                    ExpiredProgramRelays[i].SendCustomEvent(ExpiredEventNames[i]);
+                }
+            }
+        }
+
+        public override void OnPurchaseConfirmed(IProduct eventProduct, VRCPlayerApi player, bool purchased)
+        {
+            if (!player.isLocal || product == null) return;
+            if (eventProduct.ID != product.ID) return;
+
+            isOwned = true;
+
+            for (int i = 0; i < PurchasedProgramRelays.Length; i++)
+            {
+                PurchasedProgramRelays[i].SendCustomEvent(PurchasedEventNames[i]);
+            }
+        }
+
+        public override void OnPurchaseExpired(IProduct eventProduct, VRCPlayerApi player)
+        {
+            if (!player.isLocal || product == null) return;
+            if (eventProduct.ID != product.ID) return;
+
+            isOwned = false;
+
+            for (int i = 0; i < ExpiredProgramRelays.Length; i++)
+            {
+                ExpiredProgramRelays[i].SendCustomEvent(ExpiredEventNames[i]);
+            }
+        }
+    }
+}
